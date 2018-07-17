0e13d759daf5c70b34cbb18f01d5702a0349351c
Zicun Hang <koushizon@gmail.com>
2018-07-17 11:41:28 +0800

    1

---
+++ src-server/cmake-build-debug/CMakeFiles/src_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src_server.dir/main.cpp.o"
+  "src_server.pdb"
+  "src_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
